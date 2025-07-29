d438e18217df8b99a6c33af368781ff2f7d70804
Renato Fabbri <renato.fabbri@gmail.com>
2025-07-29 20:26:51 +0200

    up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/notesourcing.github.io/', // necessario per GitHub Pages
+  base: "/", // Correct for a root domain on GitHub Pages
   plugins: [react()],
 });
